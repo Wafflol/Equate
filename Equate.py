a11a637245bbc0a5db28ef1fa9dea26cab14a924
Tommy Su <tommy.00912@gmail.com>
2024-02-02 22:37:28 -0800

    First commit

---
+++ Equate.py
@@ -0,0 +1,61 @@
+from sympy import Symbol, diff, sympify
+from sympy.core.mul import Mul
+from sympy.printing.latex import latex
+from sympy.core.add import Add
+
+varList = []
+constList = []
+isTesting = False
+symList = {}
+partialsList = []
+
+if(not isTesting):
+    numVar = int(input("Enter number of variables: "))
+
+    for i in range(numVar):
+        varList.append(input("Enter next var: "))
+
+    print(varList)
+
+    numConst = int(input("Enter number of constants: "))
+
+    for i in range(numConst):
+        constList.append(input("Enter next constant: "))
+    
+    #creates symbols for each var and stores them to symList
+    for i in range(numVar):
+        symList.update({varList[i]: Symbol(varList[i], real=True)}) 
+#x, y, z = symbols('x y z', real=True)
+eq = input("Enter full equation: ")
+
+#replace all ^ with **
+eq = eq.replace("^", "**")
+
+
+print("Your equivalent equation is:")
+#print(parse_expr(eq, transformations=transformations))
+
+print(symList)
+print(type(symList["x"]))
+# Convert the string to a SymPy expression
+
+symEq = sympify(eq, evaluate=False)
+
+varSet = symEq.free_symbols
+
+for var in varSet:
+    if symList.get(str(var)) != None:
+        symList.update({str(var): var})
+
+#do partial diff
+for i in range(numVar):
+    print(diff(symEq, symList[varList[i]]), "\n")
+    partialsList.append(diff(symEq, symList[varList[i]]))
+finalEq = 0
+for i, x in enumerate(partialsList):
+    tmp = (Mul(x, Symbol("\\delta " + varList[i])))**2
+    finalEq = Add(finalEq, tmp)
+
+print("final uncertainty equation: \\sqrt{", latex(finalEq), "}")
+
+
